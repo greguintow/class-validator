219b220ce79913161d811e6ddb3604b6c18c588d
Greg Teixeira <greg.teixeira123@gmail.com>
2021-11-12 03:13:37 -0300

    fix: fix ForbidIf decorator

--- src/decorator/common/ForbidIf.ts
+++ src/decorator/common/ForbidIf.ts
@@ -1,13 +1,21 @@
-import { ValidationTypes } from '../../validation/ValidationTypes';
 import { ValidationOptions } from '../ValidationOptions';
-import { ValidateBy } from './ValidateBy';
+import { ValidationMetadataArgs } from '../../metadata/ValidationMetadataArgs';
+import { ValidationTypes } from '../../validation/ValidationTypes';
+import { ValidationMetadata } from '../../metadata/ValidationMetadata';
+import { getMetadataStorage } from '../../metadata/MetadataStorage';
 
-export function ForbidIf(condition: (object: any, value: any) => boolean, validationOptions?: ValidationOptions) {
-  return ValidateBy(
-    {
-      name: ValidationTypes.BLOCK_VALIDATION,
+export function ForbidIf(
+  condition: (object: any, value: any) => boolean,
+  validationOptions?: ValidationOptions
+): PropertyDecorator {
+  return function (object: object, propertyName: string): void {
+    const args: ValidationMetadataArgs = {
+      type: ValidationTypes.BLOCK_VALIDATION,
+      target: object.constructor,
+      propertyName: propertyName,
       constraints: [condition],
-    },
-    validationOptions
-  );
+      validationOptions: validationOptions,
+    };
+    getMetadataStorage().addValidationMetadata(new ValidationMetadata(args));
+  };
 }
